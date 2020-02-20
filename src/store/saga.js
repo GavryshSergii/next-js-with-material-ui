b428acd7494757a1908149257d267f0a2deabed4
Гавриш Сергій <s.havrysh.2107@in.np.gov.ua>
2020-02-20 16:55:33 +0200

    redux with redux-saga add polyfill fetch & promise for node

--- src/store/saga.js
+++ src/store/saga.js
@@ -1,8 +1,8 @@
 //enable if they will not work promice or fetch
-// import es6promise from 'es6-promise';
-// import 'isomorphic-unfetch';
-//
-// es6promise.polyfill();
+import es6promise from 'es6-promise';
+import 'isomorphic-unfetch';
+
+es6promise.polyfill();
 
 import { all, call, delay, put, take, takeLatest } from 'redux-saga/effects';
 import { actionTypes, failure, loadDataSuccess, tickClock } from './actions';
